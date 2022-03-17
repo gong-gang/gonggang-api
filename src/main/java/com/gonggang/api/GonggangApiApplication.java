3fc5c4f82dee2ffe168e261eec969257ba391203
jyunkim <jyunkim13@gmail.com>
2022-03-17 18:40:09 +0900

    initial commit

---
+++ src/main/java/com/gonggang/api/GonggangApiApplication.java
@@ -0,0 +1,13 @@
+package com.gonggang.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GonggangApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GonggangApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gonggang/api/GonggangApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gonggang.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GonggangApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
